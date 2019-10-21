85472ea187c52b593d413c98f4d4e4e3434f0380
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-10-20 22:15:09 -0500

    [v1.5.1]

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'BOCKC Commission',
-    'version': '1.5.0',
+    'version': '1.5.1',
     'author': 'Moisés Navarro',
     'website': 'https://github.com/AfroMonkey',
     'depends': [
